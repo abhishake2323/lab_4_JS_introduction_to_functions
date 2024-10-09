5a920d4559b8ff5044ff85c28ef28613d78a48d3
abhishake2323 <abhishakem@hotmail.com>
2024-10-09 18:36:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function greet(name) {
+    return "Hello, " + name;
+}
+let greeting = greet("Jason");
+console.log("Greeting with function greet() with 'Jason' as parameter : " + greeting);
+
+function addNumbers(num1, num2) {
+    let res = num1 + num2;
+    return res
+}
+let res = addNumbers(76, 24);
+console.log("Adding numbers using function addNumbers with parameters 76 and 24 is : " + res);
+
+let x = 10;
+console.log("Value of x is :" + x);
+function changeValue() {
+    console.log("called changeValue function");
+    let localx = 11;
+    x = localx;
+    return x;
+}
+
+
+console.log("Value changed to :" + changeValue());
+function outerFunction() {
+    console.log("called outerfunction");
+    let count = 0;
+    console.log("Count is :" + count);
+    return function () {
+        console.log("called inner function");
+        count = count + 10;
+        return count;
+    }
+}
+
+let fvar = outerFunction();
+
+console.log("updated count: " + fvar());
+console.log("updated count: " + fvar());
+console.log("updated count: " + fvar());
